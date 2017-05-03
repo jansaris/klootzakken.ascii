be90dd2d02ac78897b00978b41705963ba8c4fea
Jan Saris <jan.saris@gmail.com>
2017-05-03 18:33:25 +0200

    Created solution file with console app

---
+++ klootzakken.ascii/klootzakken.ascii/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace klootzakken.ascii
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
